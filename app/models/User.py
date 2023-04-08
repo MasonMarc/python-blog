c4fab0ef2235cd27ad4fdeb1da86355cc26848ce
MasonMarc <masonmarcantel@gmail.com>
2023-04-08 14:06:06 -0400

    added models and seeds

--- app/models/User.py
+++ app/models/User.py
@@ -1,5 +1,9 @@
 from app.db import Base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import validates
+import bcrypt
+
+salt = bcrypt.gensalt()
 
 class User(Base):
   __tablename__ = 'users'
@@ -7,3 +11,17 @@ class User(Base):
   username = Column(String(50), nullable=False)
   email = Column(String(50), nullable=False, unique=True)
   password = Column(String(100), nullable=False)
+
+  @validates('email')
+  def validate_email(self, key, email):
+    assert '@' in email 
+    # ensures @ is present in string
+
+    return email
+  
+  @validates('password')
+  def validate_password(self, key, password):
+    assert len(password) > 4
+    # ensures password is at least 5 chars
+
+    return bcrypt.hashpw(password.encode('utf-8'), salt)
